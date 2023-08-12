bcf8bb8dd1217bb9c561a403ff6cc0f0dffc7fbe
Ema Puccio <emapuccio72@gmail.com>
2023-08-12 02:34:47 -0300

    Elección de sabores en proceso

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ let nombre = prompt("Por favor, ingrese su nombre")
 
 alert(`Bienvenid@ a mi heladería ${nombre}`)
 
-//Medidas de helado
+//Medidas de helado disponibles
 const medidas = ["1/4", "1/2", "1"]
 
+//Ingreso de la cantidad del pedido
 const medidaHelado = prompt("Ingrese la cantidad de helado (1/4, 1/2, 1)")
 
-function eleccionMedida(medidaHelado) {
-    let esMedida = verificarSiNoEsMedida(medidaHelado)
-    while(esMedida){
-        esMedida = verificarSiNoEsMedida(prompt("Elija una medida válida (1/4, 1/2, 1)"))
-    }
-}
-
+//Proceso de verificación de cantidad
 const verificarSiNoEsMedida = (medida) => {
     if(medida === "1/4" || medida === "1/2" || medida === "1") {
         return false
@@ -22,7 +17,16 @@ const verificarSiNoEsMedida = (medida) => {
     }
 }
 
+function eleccionMedida(medidaHelado) {
+    let esMedida = verificarSiNoEsMedida(medidaHelado)
+    while(esMedida){
+        esMedida = verificarSiNoEsMedida(prompt("Elija una medida válida (1/4, 1/2, 1)"))
+    }
+}
+
 eleccionMedida(medidaHelado)
+
+console.log(`Su elección fue registrada: ${medidaHelado} kg`);
 
 /*switch(medidaHelado) {
     case "1/4":
@@ -44,6 +48,21 @@ eleccionMedida(medidaHelado)
 }*/
 
 //Sabores de helado
-const sabores = ["americana", "frutilla", "vainilla", "chocolate", "dulce de leche"]
+const sabores = ["americana", "frutilla", "vainilla", "chocolate", "dulce de leche", "oreo"]
+
+console.log(`Los sabores de helados disponibles son: ${sabores.join(", ")}`);
+
+//Proceso de verificación de sabores
+
+
+if(medidaHelado === "1/4"){
+    console.log("Puede elegir hasta tres sabores.");
+    const primerSaborHelado = prompt("Ingrese su primer sabor:")
+} else if(medidaHelado === "1/2") {
+    console.log("Puede elegir hasta cuatro sabores");
+} else if(medidaHelado === "1") {
+    console.log("Puede elegir hasta cinco sabores");
+}
+
 
 //let loop = confirm("Quiere elegir")
