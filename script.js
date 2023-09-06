524b48f47cc56527c0ad94d09eac3db6c1d2d3a5
Ema Puccio <emapuccio72@gmail.com>
2023-09-06 00:29:19 -0300

    Form hecho

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function pedirNombre(){
 }
 
 //En caso de ya haber ingresado
+/*
 function saludar(nombre){
     alert(`Bienvenid@ de nuevo a mi heladería ${nombre}`)
 }
@@ -17,6 +18,7 @@ if(conseguirNombre){
 } else {
     pedirNombre()
 }
+*/
 
 //Medidas de helado disponibles
 const medidas = ["1/4", "1/2", "1"]
@@ -83,12 +85,12 @@ function eleccionTodosLosSabores(numeroDeSabores) {
 //INGRESO DE DATOS DEL USUARIO
 
 //Ingreso de la cantidad del pedido
-let medidaHelado = eleccionMedida()
+//let medidaHelado = eleccionMedida()
 
-console.log(`Su elección fue registrada: ${medidaHelado} kg`);
+//console.log(`Su elección fue registrada: ${medidaHelado} kg`);
 
 //Ingreso de la cantidad de sabores y gustos
-console.log(`Los sabores de helados disponibles son: ${sabores.join(", ")}`);
+/*console.log(`Los sabores de helados disponibles son: ${sabores.join(", ")}`);
 
 let nroSaboresElegidos
 let saboresElegidos
@@ -112,3 +114,114 @@ if(medidaHelado === "1/4"){
 
 //PEDIDO FINAL
 alert(`El pedido final es: ${medidaHelado} kg con los sabores ${saboresElegidos.join(", ")}`);
+*/
+
+//NUEVO
+const productos = []
+
+class Helado {
+    constructor(medida, sabores) {
+        this.medida = medida
+        this.sabores = sabores
+    }
+}
+
+function armarHelado(){
+
+}
+
+const formMedidaHelado = document.getElementById("form-medida")
+/* formMedidaHelado.addEventListener("submit", (evento) => {
+    evento.preventDefault(0);
+    const data = new FormData(evento.target);
+    console.log(data);
+    const heladoSinProcesar = Object.fromEntries(data);
+  
+    //Funcion para agregar sabores de heladoSinProcesar elegidos a un array.
+    let saboresElegidos = [];
+    const crearArray = () => {
+      //const sabores = ["americana", "frutilla", "vainilla", "chocolate", "dulce de leche", "oreo"]
+      sabores.forEach((sabor) => {
+        if (heladoSinProcesar.sabor == "on") {
+          saboresElegidos.push(sabor);
+        }
+        //console.log(heladoSinProcesar.sabor);
+        //console.log(sabor);
+      });
+    };
+    crearArray();
+  
+    const helado = new Helado(heladoSinProcesar.medida, saboresElegidos);
+    console.log(heladoSinProcesar); //{medida: '1/4', americana: 'on', chocolate: 'on', dulce de leche: 'on'}
+    console.log(helado)
+  }); */
+
+  formMedidaHelado.addEventListener("submit", (event) => {
+    event.preventDefault(0);
+    const medidaHelado = document.getElementById("medida-helado").value;
+    const saborAmericana = document.getElementById("americana");
+    const saborFrutilla = document.getElementById("frutilla");
+    const saborVainilla = document.getElementById("vainilla");
+    const saborChocolate = document.getElementById("chocolate");
+    const saborDulceDeLeche = document.getElementById("dulce-de-leche");
+    const saborOreo = document.getElementById("oreo");
+  
+    const checkSabores = [
+      saborAmericana,
+      saborFrutilla,
+      saborVainilla,
+      saborChocolate,
+      saborDulceDeLeche,
+      saborOreo,
+    ];
+  
+    let saboresElegidos = [];
+    checkSabores.forEach((sabor) => {
+      if (sabor.checked) {
+        saboresElegidos.push(sabor.id);
+      }
+    });
+  
+    const helado = new Helado(medidaHelado, saboresElegidos);
+    console.log(helado);
+  });
+  
+
+
+const carrito = document.querySelector(".carrito")
+carrito.innerHTML = `
+
+`
+
+
+
+
+
+
+
+//
+const renderizarCarrito = ()=>{
+    // borra el cotnenido de carrito y renderiza carrito en una lista
+    const listaCarrito = document.getElementById("listaCarrito")
+    // borramos para evitar clones viejos
+    listaCarrito.innerHTML=""
+    carrito.forEach(({name, price, quantity, id}) =>{
+        let elementoLista = document.createElement("li")
+        elementoLista.innerHTML=`Producto:${name} -- P/u: ${price} -- Cant.:${quantity} <button id="eliminarCarrito${id}">X</button>`
+        listaCarrito.appendChild(elementoLista)
+        const botonBorrar = document.getElementById(`eliminarCarrito${id}`)
+        botonBorrar.addEventListener("click",()=>{
+            // creo un array sin el elemento a borrar y lo igualo a carrito
+            carrito = carrito.filter((elemento)=>{
+                if(elemento.id !== id){
+                    return elemento
+                }
+            })
+            let carritoString = JSON.stringify(carrito)
+            localStorage.setItem("carrito", carritoString)
+            renderizarCarrito()
+        })
+        let carritoString = JSON.stringify(carrito)
+        localStorage.setItem("carrito", carritoString)
+    })
+}
