cdc71f713c0438bad73dca52e00eba4cd09fc68f
Ema Puccio <emapuccio72@gmail.com>
2023-09-10 20:52:33 -0300

    Pequeños cambios y agrego comentarios

--- script.js
+++ script.js
@@ -1,122 +1,3 @@
-//Petición de nombre
-function pedirNombre(){
-    const nombre = prompt("Por favor, ingrese su nombre")
-    localStorage.setItem("nombreUsuario", nombre)
-    alert(`Bienvenid@ ${nombre}`)
-}
-
-//En caso de ya haber ingresado
-/*
-function saludar(nombre){
-    alert(`Bienvenid@ de nuevo a mi heladería ${nombre}`)
-}
-
-let conseguirNombre = localStorage.getItem("nombreUsuario")
-
-if(conseguirNombre){
-    saludar(conseguirNombre)
-} else {
-    pedirNombre()
-}
-*/
-
-//Medidas de helado disponibles
-const medidas = ["1/4", "1/2", "1"]
-
-//Proceso de verificación de cantidad
-const verificarSiNoEsMedida = (medida) => {
-    if(medida === "1/4" || medida === "1/2" || medida === "1") {
-        return false
-    } else {
-        return true
-    }
-}
-
-function eleccionMedida() {
-    let elegirMedida = prompt("Ingrese la cantidad de helado (1/4, 1/2, 1)")
-    let noEsMedida = verificarSiNoEsMedida(elegirMedida)
-    while(noEsMedida){
-        elegirMedida = prompt("Elija una medida válida (1/4, 1/2, 1)")
-        noEsMedida = verificarSiNoEsMedida(elegirMedida)
-    }
-    return elegirMedida
-}
-
-//Sabores de helado
-const sabores = ["americana", "frutilla", "vainilla", "chocolate", "dulce de leche", "oreo"]
-
-//Proceso de verificación de sabores
-const verificarSiNoEsSabor = (sabor) => {
-    if(sabores.indexOf(sabor) >= 0) {
-        return false
-    } else {
-        return true
-    }
-}
-
-function eleccionSabor() {
-    let elegirSabor = prompt("Ingrese sabor:").toLowerCase()
-    let esSabor = verificarSiNoEsSabor(elegirSabor)
-    while(esSabor) {
-        elegirSabor = prompt("Elija un sabor incluido en nuestro menú")
-        esSabor = verificarSiNoEsSabor(elegirSabor)
-    }
-    return elegirSabor
-}
-
-//Proceso de elección de cantidad de sabores
-// function cuantosSabores(maximoSabores) {
-//     let elegirNumeroSabores = parseInt(prompt("Ingrese la cantidad de gustos a elegir"))
-//     while(elegirNumeroSabores <= 0 || elegirNumeroSabores > maximoSabores){
-//         elegirNumeroSabores = parseInt(prompt(`Ingrese una cantidad de gustos válida (hasta ${maximoSabores})`))
-//     }
-//     return elegirNumeroSabores
-// }
-
-//Bucle para elección de sabores según cantidad
-function eleccionTodosLosSabores(numeroDeSabores) {
-    let elecciones = []
-    for(let i = 1; i <= numeroDeSabores; i++){
-        elecciones.push(eleccionSabor())
-    }
-    return elecciones
-}
-
-//INGRESO DE DATOS DEL USUARIO
-
-//Ingreso de la cantidad del pedido
-//let medidaHelado = eleccionMedida()
-
-//console.log(`Su elección fue registrada: ${medidaHelado} kg`);
-
-//Ingreso de la cantidad de sabores y gustos
-/*console.log(`Los sabores de helados disponibles son: ${sabores.join(", ")}`);
-
-let nroSaboresElegidos
-let saboresElegidos
-
-if(medidaHelado === "1/4"){
-    console.log("Puede elegir hasta tres sabores.");
-    nroSaboresElegidos = cuantosSabores(3)
-    saboresElegidos = eleccionTodosLosSabores(nroSaboresElegidos)
-
-} else if(medidaHelado === "1/2") {
-    console.log("Puede elegir hasta cuatro sabores");
-    nroSaboresElegidos = cuantosSabores(4)
-    saboresElegidos = eleccionTodosLosSabores(nroSaboresElegidos)
-
-} else if(medidaHelado === "1") {
-    console.log("Puede elegir hasta cinco sabores");
-    nroSaboresElegidos = cuantosSabores(5)
-    saboresElegidos = eleccionTodosLosSabores(nroSaboresElegidos)
-
-}
-
-//PEDIDO FINAL
-alert(`El pedido final es: ${medidaHelado} kg con los sabores ${saboresElegidos.join(", ")}`);
-*/
-
-//NUEVO
 let productos = []
 
 class Helado {
@@ -134,7 +15,7 @@ class Helado {
         return this.sabores
     }
     get conseguirHelado() {
-        return `Producto: Helado de ${this.medida} kg con los sabores: ${this.sabores.join(`, `)}`
+        return `Producto: Helado de ${this.medida} kg con los sabores: ${(this.sabores).join(", ")}`
     }
 }
 
@@ -156,7 +37,7 @@ const crearCarrito = () => {
         
         //Guarda el array de productos
         productosJSON = JSON.stringify(productos)
-        sessionStorage.setItem("productos", productosJSON)
+        localStorage.setItem("productos", productosJSON)
         
         //Recupera el botón creado en HTML de eliminar producto
         const botonEliminar = document.getElementById(`item${idHelado}`);
@@ -171,24 +52,24 @@ const crearCarrito = () => {
             }
         //Guarda nuevamente el array de productos si se borró alguno
         productosJSON = JSON.stringify(productos)
-        sessionStorage.setItem("productos", productosJSON)
+        localStorage.setItem("productos", productosJSON)
         });
     }
 }
 
+//Función para recuperar array de productos en el storage si es que hay
 function buscarCarritoEnStorage() {
-    let productosJS = JSON.parse(sessionStorage.getItem("productos"))
+    //Recupera el localStorage
+    let productosJS = JSON.parse(localStorage.getItem("productos"))
 
+    //Si no estaba vacío
     if(productosJS) {  
         let carritoViejo = []
-
         for(const item of productosJS) {
             let helado = new Helado(item.id, item.medida, item.sabores)
             carritoViejo.push(helado)
         }
-
         productos.push(...carritoViejo)
-
     } 
 }
 
@@ -229,14 +110,15 @@ formMedidaHelado.addEventListener("submit", (event) => {
     const helado = new Helado(contador, medidaHelado, saboresElegidos);
     productos.push(helado);
 
+    //Guarda en el Storage los productos recibidos
     productosJSON = JSON.stringify(productos);
-    sessionStorage.setItem("productos", productosJSON);
+    localStorage.setItem("productos", productosJSON);
 
-    //Ejecuta 
+    //Actualiza el carrito
     crearCarrito()
 });
 
 //Busca si había previamente algún elemento en el carrito
 buscarCarritoEnStorage()
-//Actualiza el carrito
+//Actualiza carrito
 crearCarrito()
