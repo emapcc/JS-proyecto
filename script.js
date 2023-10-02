dd40d48c27f0739e5729be81b504c871e01a6c33
Ema Puccio <emapuccio72@gmail.com>
2023-10-02 03:09:30 -0300

    Agrego asincronismo y promesas

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let productos = []
 
 class Helado {
-    constructor(numero, medida, sabores) {
+    constructor(numero, medida, sabores, precio) {
         //Propiedades del objeto
         this.id = numero
         this.medida = medida
         this.sabores = sabores
+        this.precio = precio
     }
     //Métodos del objeto
     get conseguirMedida() {
@@ -15,7 +16,7 @@ class Helado {
         return this.sabores
     }
     get conseguirHelado() {
-        return `Helado de ${this.medida} kg con los sabores: ${(this.sabores).join(", ")}`
+        return `Helado de ${this.medida} kg con los sabores: ${(this.sabores).join(", ")} --- $${this.precio}`
     }
 }
 
@@ -29,7 +30,9 @@ const crearCarrito = () => {
         let item = producto.conseguirHelado
         let idHelado = producto.id
         let itemCarrito = document.createElement("li")
-        itemCarrito.innerHTML = `<p id="helado${idHelado}">${item}<button id="item${idHelado}">Eliminar</button></p>`
+        itemCarrito.innerHTML = `
+        <p id="helado${idHelado}">${item}<button id="item${idHelado}">Eliminar</button></p>
+        `
         carrito.appendChild(itemCarrito)
         
         //Guarda el array de productos
@@ -59,7 +62,7 @@ function buscarCarritoEnStorage() {
     if(productosJS) {  
         let carritoViejo = []
         for(const item of productosJS) {
-            let helado = new Helado(item.id, item.medida, item.sabores)
+            let helado = new Helado(item.id, item.medida, item.sabores, item.precio)
             carritoViejo.push(helado)
         }
         productos.push(...carritoViejo)
@@ -156,7 +159,7 @@ formMedidaHelado.addEventListener("submit", (event) => {
                     title: 'Excede el límite de sabores'
                 })
             } else {
-                const helado = new Helado(contador, medidaHelado, saboresElegidos);
+                const helado = new Helado(contador, medidaHelado, saboresElegidos, "800");
                 productos.push(helado);
             }
         } else if (medidaHelado == "1/2") {
@@ -178,7 +181,7 @@ formMedidaHelado.addEventListener("submit", (event) => {
                     title: 'Excede el límite de sabores'
                 })
             } else {
-                const helado = new Helado(contador, medidaHelado, saboresElegidos);
+                const helado = new Helado(contador, medidaHelado, saboresElegidos, "1000");
                 productos.push(helado);
             }
         } else if (medidaHelado == "1") {
@@ -200,7 +203,7 @@ formMedidaHelado.addEventListener("submit", (event) => {
                     title: 'Excede el límite de sabores'
                 })
             } else {
-                const helado = new Helado(contador, medidaHelado, saboresElegidos);
+                const helado = new Helado(contador, medidaHelado, saboresElegidos, "1200");
                 productos.push(helado);
             }
         }
@@ -269,6 +272,110 @@ formDatosUsuario.addEventListener("submit", (evento) => {
         })
     }
 })
+
+//Función para recuperar promociones
+let promocion1 = []
+let promocion2 = []
+
+function mostrarPromociones() {
+    let containerPromo1 = document.getElementById("container-promo1")
+    let containerPromo2 = document.getElementById("container-promo2")
+
+    //Sección de la promoción 1
+    let precioPromo1 = 0
+    promocion1.forEach((helado) => {
+        precioPromo1 += helado.precio
+        let divPromo1 = document.createElement(`li`)
+        divPromo1.innerHTML = `
+        <p>Un helado de ${helado.medida}kg con los sabores ${(helado.sabores).join(", ")}</p>
+        `
+        containerPromo1.appendChild(divPromo1)
+    })
+
+    let sectionPromo1 = document.getElementById(`section-promo1`)
+    let precioTotalPromo1 = document.createElement(`div`)
+    precioTotalPromo1.innerHTML = `
+    <p id="total-promo">Total: $${precioPromo1}</p>
+    <p>(En caso de querer eliminar un helado se eliminarán ambos debido a la promoción)</p>
+    <button id="agregar-promo1">Agregar</button>
+    `
+    sectionPromo1.appendChild(precioTotalPromo1)
+
+    //Evento para agregar promo1 al carrito
+    let buttonAgregarPromo1 = document.getElementById(`agregar-promo1`)
+    buttonAgregarPromo1.addEventListener("click", () => {
+        for(const helado of promocion1){
+            productos.push(helado)
+        }
+        productosJSON = JSON.stringify(productos);
+        localStorage.setItem("productos", productosJSON);
+        crearCarrito()
+    })
+
+    //Sección de la promoción 2
+    let precioPromo2 = 0
+    promocion2.forEach((helado) => {
+        precioPromo2 += helado.precio
+        let divPromo2 = document.createElement(`li`)
+        divPromo2.innerHTML = `
+        <p>Un helado de ${helado.medida}kg con los sabores ${(helado.sabores).join(", ")}</p>
+        `
+        containerPromo2.appendChild(divPromo2)
+    })
+
+    let sectionPromo2 = document.getElementById(`section-promo2`)
+    let precioTotalPromo2 = document.createElement(`div`)
+    precioTotalPromo2.innerHTML = `
+    <p id="total-promo">Total: $${precioPromo2}</p>
+    <p>(En caso de querer eliminar un helado se eliminarán ambos debido a la promoción)</p>
+    <button id="agregar-promo2">Agregar</button>
+    `
+    sectionPromo2.appendChild(precioTotalPromo2)
+
+    //Evento para agregar promo1 al carrito
+    let buttonAgregarPromo2 = document.getElementById(`agregar-promo2`)
+    buttonAgregarPromo2.addEventListener("click", () => {
+        for(const helado of promocion2){
+            productos.push(helado)
+        }
+        productosJSON = JSON.stringify(productos);
+        localStorage.setItem("productos", productosJSON);
+        crearCarrito()
+    })
+}
+
+async function recuperarPromocion() {
+    try{
+        let promo1SinProcesar = await fetch("./promocion1.json")
+        let promo2SinProcesar = await fetch("./promocion2.json")
+        let promo1 = await promo1SinProcesar.json()
+        let promo2 = await promo2SinProcesar.json()
+
+        for(const elemento of promo1){
+            let idHelado = elemento.id
+            let medidaHelado = elemento.medida
+            let saboresHelado = elemento.sabores
+            let precioHelado = elemento.precio
+            let helado = new Helado(idHelado, medidaHelado, saboresHelado, precioHelado)
+            promocion1.push(helado)
+        }
+        for(const elemento of promo2){
+            let idHelado = elemento.id
+            let medidaHelado = elemento.medida
+            let saboresHelado = elemento.sabores
+            let precioHelado = elemento.precio
+            let helado = new Helado(idHelado, medidaHelado, saboresHelado, precioHelado)
+            promocion2.push(helado)
+        }
+
+        mostrarPromociones()
+
+    } catch(error){
+        console.warn(error)
+    }
+}
+
+recuperarPromocion()
 
 //Busca si había previamente algún elemento en el carrito
 buscarCarritoEnStorage()
